289e09de2a07f386a10ee76a6e73b1eb5d4d67a9
FelysNeko <88437453+FelysNeko@users.noreply.github.com>
2024-09-16 13:56:23 +0800

    update

--- parser/src/expr/mod.rs
+++ parser/src/expr/mod.rs
@@ -1,2 +1,28 @@
+use crate::expr::registry::Method;
+use crate::shared::ast::ElyPrimary;
+use daybreak::Parser;
+use std::fmt::{Debug, Display};
+use std::hash::Hash;
+
 mod registry;
 mod parser;
+
+
+pub fn expr<CT, CR>(other: &mut Parser<CT, CR>) -> Option<ElyPrimary>
+where
+    CT: Display + Debug + Hash + PartialEq + Eq + Clone + Copy,
+    CR: Display + Debug + Clone,
+{
+    let mut parser = other.export();
+    let result = parser.ely_primary()?;
+    let pos = parser.stream.mark();
+    other.stream.jump(pos);
+    Some(result)
+}
+
+#[test]
+fn test() {
+    let mut x = Parser::<i32, i32>::new("123.0");
+    let result = expr(&mut x);
+    println!("{:?}", result)
+}
--- parser/src/expr/parser.rs
+++ parser/src/expr/parser.rs
@@ -1,7 +1,7 @@
-use crate::shared::ast::*;
 use crate::expr::registry::*;
-use daybreak::Parser;
+use crate::shared::ast::*;
 use crate::string;
+use daybreak::Parser;
 
 impl Base for Parser<'_, CacheType, CacheResult> {
     type CT = CacheType;
@@ -12,7 +12,7 @@ impl Method for Parser<'_, CacheType, CacheResult> {
     fn ely_expression(&mut self) -> Option<ElyExpression> {
         todo!()
     }
-    
+
     fn ely_disjunction(&mut self) -> Option<ElyDisjunction> {
         todo!()
     }
@@ -132,20 +132,170 @@ impl Method for Parser<'_, CacheType, CacheResult> {
         None
     }
 
+    #[daybreak::strict]
+    #[daybreak::memoize(ElyName)]
     fn ely_name(&mut self) -> Option<ElyName> {
-        todo!()
+        let pos = self.stream.mark();
+        if let Some(body) = || -> Option<ElyName> {
+            let first = self.scan(|c| c.is_ascii_alphabetic() || c == '_')?;
+            self.stream.strict(true);
+            let mut body = String::from(first);
+            while let Some(more) = self.scan(|c| c.is_ascii_alphanumeric() || c == '_') {
+                body.push(more)
+            }
+            Some(body)
+        }() {
+            return Some(body);
+        } else {
+            self.stream.jump(pos)
+        }
+        None
     }
 
+    #[daybreak::strict]
+    #[daybreak::memoize(ElyInteger)]
     fn ely_integer(&mut self) -> Option<ElyInteger> {
-        todo!()
+        let pos = self.stream.mark();
+        let mut cut = false;
+        if let Some(body) = || -> Option<ElyInteger> {
+            self.expect("0x")?;
+            self.stream.strict(true);
+            cut = true;
+            let first = self.scan(|c| c.is_ascii_hexdigit())?;
+            let mut body = String::from(first);
+            while let Some(more) = self.scan(|c| c.is_ascii_hexdigit()) {
+                body.push(more)
+            }
+            Some(ElyInteger::Base16(body))
+        }() {
+            return Some(body);
+        } else {
+            self.stream.jump(pos)
+        }
+        if cut { return None; }
+        if let Some(body) = || -> Option<ElyInteger> {
+            self.expect("0o")?;
+            self.stream.strict(true);
+            cut = true;
+            let first = self.scan(|c| matches!(c, '0'..='7'))?;
+            let mut body = String::from(first);
+            while let Some(more) = self.scan(|c| matches!(c, '0'..='7')) {
+                body.push(more)
+            }
+            Some(ElyInteger::Base8(body))
+        }() {
+            return Some(body);
+        } else {
+            self.stream.jump(pos)
+        }
+        if cut { return None; }
+        if let Some(body) = || -> Option<ElyInteger> {
+            self.expect("0b")?;
+            self.stream.strict(true);
+            cut = true;
+            let first = self.scan(|c| matches!(c, '0'|'1'))?;
+            let mut body = String::from(first);
+            while let Some(more) = self.scan(|c| matches!(c, '0'|'1')) {
+                body.push(more)
+            }
+            Some(ElyInteger::Base2(body))
+        }() {
+            return Some(body);
+        } else {
+            self.stream.jump(pos)
+        }
+        if cut { return None; }
+        if let Some(body) = || -> Option<ElyInteger> {
+            self.expect("0")?;
+            cut = true;
+            self.lookahead(|c| !c.is_ascii_digit())?;
+            Some(ElyInteger::Base10("0".to_string()))
+        }() {
+            return Some(body);
+        } else {
+            self.stream.jump(pos)
+        }
+        if cut { return None; }
+        if let Some(body) = || -> Option<ElyInteger> {
+            let first = self.scan(|c| c.is_ascii_digit())?;
+            self.stream.strict(true);
+            let mut body = String::from(first);
+            while let Some(more) = self.scan(|c| c.is_ascii_digit()) {
+                body.push(more)
+            }
+            Some(ElyInteger::Base10(body))
+        }() {
+            return Some(body);
+        } else {
+            self.stream.jump(pos)
+        }
+        None
     }
 
+    #[daybreak::strict]
+    #[daybreak::memoize(ElyDecimal)]
     fn ely_decimal(&mut self) -> Option<ElyDecimal> {
-        todo!()
+        let pos = self.stream.mark();
+        let mut cut = false;
+        if let Some(body) = || -> Option<ElyDecimal> {
+            self.expect("0")?;
+            self.stream.strict(true);
+            cut = true;
+            self.expect(".")?;
+            let first = self.scan(|c| c.is_ascii_digit())?;
+            let mut frac = String::from(first);
+            while let Some(more) = self.scan(|c| c.is_ascii_digit()) {
+                frac.push(more)
+            }
+            Some(ElyDecimal { whole: "0".to_string(), frac })
+        }() {
+            return Some(body);
+        } else {
+            self.stream.jump(pos)
+        }
+        if cut { return None; }
+        if let Some(body) = || -> Option<ElyDecimal> {
+            let first = self.scan(|c| c.is_ascii_digit())?;
+            self.stream.strict(true);
+            let mut whole = String::from(first);
+            while let Some(more) = self.scan(|c| c.is_ascii_digit()) {
+                whole.push(more)
+            }
+            self.expect(".")?;
+            let first = self.scan(|c| c.is_ascii_digit())?;
+            let mut frac = String::from(first);
+            while let Some(more) = self.scan(|c| c.is_ascii_digit()) {
+                frac.push(more)
+            }
+            Some(ElyDecimal { whole, frac })
+        }() {
+            return Some(body);
+        } else {
+            self.stream.jump(pos)
+        }
+        None
     }
 
+    #[daybreak::memoize(ElyBoolean)]
     fn ely_boolean(&mut self) -> Option<ElyBoolean> {
-        todo!()
+        let pos = self.stream.mark();
+        if let Some(body) = || -> Option<ElyBoolean> {
+            self.expect("true")?;
+            Some(ElyBoolean::True)
+        }() {
+            return Some(body);
+        } else {
+            self.stream.jump(pos)
+        }
+        if let Some(body) = || -> Option<ElyBoolean> {
+            self.expect("false")?;
+            Some(ElyBoolean::False)
+        }() {
+            return Some(body);
+        } else {
+            self.stream.jump(pos)
+        }
+        None
     }
 
     #[daybreak::memoize(ElyExpect)]
